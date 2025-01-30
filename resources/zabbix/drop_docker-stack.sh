41992bb147ee3e2010206ccda9157e383a358df0
toysavi <savi_toy@outlook.com>
2025-01-30 22:47:40 +0700

    update

--- resources/zabbix/drop_docker-stack.sh
+++ resources/zabbix/drop_docker-stack.sh
@@ -8,23 +8,24 @@ fi
 
 STACK_NAME=$1
 
+# Ask for confirmation to delete the stack
+read -p "Are you sure you want to delete the stack $STACK_NAME? (yes/no): " CONFIRM
+if [[ "$CONFIRM" != "yes" ]]; then
+    echo "Aborting."
+    exit 1
+fi
+
 # Remove the Docker stack
-docker stack rm $STACK_NAME
+docker stack rm "$STACK_NAME"
 
 # Wait for the stack to be removed
 echo "Waiting for stack $STACK_NAME to be removed..."
-while docker stack ls | grep -q $STACK_NAME; do
+while docker stack ls | grep -q "$STACK_NAME"; do
     sleep 1
 done
 
 echo "Stack $STACK_NAME has been removed."
+
 # List current Docker stacks
 echo "Current Docker stacks:"
 docker stack ls
-
-# Ask for confirmation to delete the stack
-read -p "Are you sure you want to delete the stack $STACK_NAME? (y/n): " CONFIRM
-if [ "$CONFIRM" != "y" ]; then
-    echo "Aborting."
-    exit 1
-fi
